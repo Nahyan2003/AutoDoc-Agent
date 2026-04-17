71b037e1cd02eea6644a4915975839e49e342258
MOHAMMED NAHYAN ASHRAF <nahyanashraf3878@gmail.com>
2026-04-17 21:34:57 +0530

    Final attempt after fixing GitHub permissions

--- sample_code.py
+++ sample_code.py
@@ -1,12 +1,3 @@
+# Updated version 2.0
 def celsius_to_fahrenheit(celsius):
-    """Converts Celsius to Fahrenheit."""
-    return (celsius * 9/5) + 32
-
-def fahrenheit_to_celsius(fahrenheit):
-    """Converts Fahrenheit to Celsius."""
-    return (fahrenheit - 32) * 5/9
-
-# Example Usage
-current_temp_dubai = 35  # Celsius
-print(f"The temperature in Dubai is {current_temp_dubai}°C")
-print(f"In Fahrenheit, that is {celsius_to_fahrenheit(current_temp_dubai)}°F")
+    ...
